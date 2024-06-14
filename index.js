2614c0c1f781c2f83b263ad81c5d0b73bbdc6a00
Hello CoderBoy <mahmudhasan.hb@gmail.com>
2024-06-14 17:55:43 +0600

    added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
-const AWS = require("aws-sdk")
+// const AWS = require("@aws-sdk/client-dynamodb")
+const { DynamoDBClient, ListBackupsCommand } = require("@aws-sdk/client-dynamodb");
 const app = express()
 const port = 2000;
 app.use(express.json())
@@ -8,25 +9,29 @@ app.get("/", async (req, res) => {
     res.send({ message: "welcome to our server" })
 })
 
-
-const DB = AWS.config.update({
-    accessKeyId: 'AKIAZQ3DUD2OQZLANC5O',
-    secretAccessKey: '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',
-    region: "us-east-1",
-})
+const client = new DynamoDBClient({
+    // accessKeyId: 'AKIAZQ3DUD2OQZLANC5O',
+    // secretAccessKey: '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',
+    // region: "us-east-1"
+});
+// AWS.config.update({
+//     accessKeyId: 'AKIAZQ3DUD2OQZLANC5O',
+//     secretAccessKey: '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',
+//     region: "us-east-1",
+// })
 
 // if(DB?.connect){
 //     console.log("connected to dynamoDB")
 // }
 
-const db = new AWS.DynamoDB.DocumentClient();
+// const db = new AWS.DynamoDB.Docum    entClient();
 
-app.get("/users", async(req,res)=>{
+app.get("/users", async (req, res) => {
     const params = {
-        TableName : "blog-site"
+        TableName: "blog-site"
     }
 
-    const data = await db.get(params).promise()
+    const data = await client.
     console.log(data);
     res.send(data)
 
